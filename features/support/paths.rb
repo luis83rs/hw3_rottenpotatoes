4d4d0535a50616eee7c6a3cfcd6189fa6011b4b4
luis83rs <luis.rs1983@gmail.com>
2013-04-21 10:53:05 +0200

    Homework part 1

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,4 +1,4 @@
-# TL;DR: YOU SHOULD DELETE THIS FILE
+  # TL;DR: YOU SHOULD DELETE THIS FILE
 #
 # This file is used by web_steps.rb, which you should also delete
 #
@@ -13,8 +13,11 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
-    when /^the movies page$/ then '/movies'
+    #when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
+    #when /^the movies page$/ then '/movies'
+      
+    when /^the home\s?page$/ then '/'
+    when /^the RottenPotatoes home page/ then '/movies'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
